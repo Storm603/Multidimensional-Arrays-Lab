fc4dc108aca02525a1c1e5786e27b35dfc564945
Storm603 <93555141+Storm603@users.noreply.github.com>
2022-01-12 00:22:55 +0200

    Add files via upload

---
+++ T05. Square With Maximum Sum/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace T05._Square_With_Maximum_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            int maxSum = int.MinValue;
+            int[] savedIndex = new int[2];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = input[j];
+                }
+            }
+
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    int sum = matrix[row, col] + matrix[row + 1, col] + matrix[row, col + 1] + matrix[row + 1, col + 1];
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        savedIndex[0] = row;
+                        savedIndex[1] = col;
+                    }
+                }
+            }
+
+            for (int i = savedIndex[0]; i < savedIndex[0] + 2; i++)
+            {
+                for (int j = savedIndex[1]; j < savedIndex[1] + 2; j++)
+                {
+                    Console.Write(matrix[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
